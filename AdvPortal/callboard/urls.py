81a170a970ca3584d9506f86d95135fc1aa4e186
Rasen Nikov <rasen800@gmail.com>
2024-04-07 21:37:20 +0600

    Обновление

--- AdvPortal/callboard/urls.py
+++ AdvPortal/callboard/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from callboard.views.index import Index
+from callboard.views.confirm import ConfirmUser, UserProfileView
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),  # http://127.0.0.1:8027
+
+    path('confirm/', ConfirmUser.as_view(), name='confirm_user'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
+    path('account/', Index.as_view(), name='index'),
 ]
